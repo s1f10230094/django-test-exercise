7b0e0675c516c71855f78f4a1207e0193445f0a0
1F10230094 <s1f102300942@iniad.org>
2024-07-04 13:42:38 +0900

    警告の対策

--- todo/tests.py
+++ todo/tests.py
@@ -3,15 +3,16 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
-        task = Task(title='task1', due_at =due)
+        task = Task(title='task1', due_at=due)
         task.save()
 
         task = Task.objects.get(pk=task.pk)
@@ -77,6 +78,7 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
+
 
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
