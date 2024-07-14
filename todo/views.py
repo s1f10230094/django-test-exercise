8a7eebe6d1f17e6260fda217fdd1d319c36b368b
1F10230094 <s1f102300942@iniad.org>
2024-07-14 19:58:16 +0900

    修正(conflict解消用)

--- todo/views.py
+++ todo/views.py
@@ -35,7 +35,7 @@ def detail(request, task_id):
 
 
 def update(request, task_id):
-      try:
+    try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
